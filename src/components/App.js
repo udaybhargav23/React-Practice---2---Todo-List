6af36b63ed4747619f92a438f4cd9748f79a636a
HETU PATEL <hetupatel.patidar@gmail.com>
2021-08-13 03:12:54 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,126 @@
-import React from "react";
-import "./../styles/App.css";
+import React, { useState } from 'react';
+import './../styles/App.css';
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+   const [note, setNote] = useState('');
+   const [editNote, setEditNote] = useState('');
+   const [items, setItems] = useState([]);
+   const [globalId, setGlobalId] = useState(0);
+   const [editId, setEditId] = useState(-1);
+
+   const handleNote = (e) => {
+      setNote(e.target.value);
+   };
+
+   const handleEditNote = (e) => {
+      setEditNote(e.target.value);
+   };
+
+   const addItem = () => {
+      if (!note) {
+         alert('Please something add in note...');
+         return;
+      }
+      items.push({ id: globalId, note });
+      setGlobalId((prevId) => prevId + 1);
+      setNote('');
+   };
+
+   const editItem = (e) => {
+      if (!editNote) {
+         alert('Empty not allowed');
+         return;
+      }
+      const itemId = e.target.value;
+      const updatedItems = items.map((item) => {
+         if (item.id == itemId) {
+            item.note = editNote;
+         }
+
+         return item;
+      });
+      setItems(updatedItems);
+
+      setEditId(-1);
+      setEditNote('');
+   };
+
+   const removeItem = (e) => {
+      const temp = items.filter((item) => item.id != e.target.value);
+      setItems(temp);
+   };
+
+   const HandleEditTextarea = (e) => {
+      const itemId = e.target.value;
+
+      for (let i = 0; i < items.length; i++) {
+         if (items[i].id == itemId) {
+            setEditNote(items[i].note);
+            break;
+         }
+      }
+
+      setEditId(itemId);
+   };
+
+   return (
+      <div id="main">
+         <h1>TODO List</h1>
+
+         <div className="todo-list">
+            <div className="text-area">
+               <textarea name="" id="task" value={note} onChange={handleNote}></textarea>
+               <br />
+               <button id="btn" onClick={addItem}>
+                  Add
+               </button>
+            </div>
+            <div className="items">
+               <h2>Todo Items</h2>
+               <ul>
+                  {items.map((item) => (
+                     <li className="list" key={item.id}>
+                        {editId == item.id && (
+                           <div className="edit-item">
+                              <textarea
+                                 name=""
+                                 id="task"
+                                 value={editNote}
+                                 onChange={handleEditNote}
+                              ></textarea>
+                              <br />
+                              <button
+                                 id="btn"
+                                 className="saveTask"
+                                 value={item.id}
+                                 onClick={editItem}
+                              >
+                                 Save
+                              </button>
+                           </div>
+                        )}
+                        <div className="list-content">
+                           <span>{item.note}</span>
+                           <div className="actions">
+                              <button
+                                 className="editTask"
+                                 value={item.id}
+                                 onClick={HandleEditTextarea}
+                              >
+                                 Edit
+                              </button>
+                              <button value={item.id} onClick={removeItem}>
+                                 Delete
+                              </button>
+                           </div>
+                        </div>
+                     </li>
+                  ))}
+               </ul>
+            </div>
+         </div>
+      </div>
+   );
 }
-
 
 export default App;
